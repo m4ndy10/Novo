9cf1ce7c5a89d8653f1cee9d7c7d52f252d7b6ad
Amanda <amanda.leite10@escola.pr.gov.br>
2026-06-03 08:46:22 -0300

    .

--- main.js
+++ main.js
@@ -15,7 +15,16 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-06-02T00:00:00")
-let tempoAtual = new Date()
+const tempoObjetivo1 = new Date("2026-12-18T00:00:00")
 
-contadores[0].textContent = tempoObjetivo1 ;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date()
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+return dias+ " dias " + horas + " horas + minutos +" minutos " + segundos + " segundos "
+}
