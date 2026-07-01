56a5422ed8d4ebede14bf0f8612a40d1c0d130c0
Amanda <amanda.leite10@escola.pr.gov.br>
2026-07-01 08:38:30 -0300

    .

--- main.js
+++ main.js
@@ -22,14 +22,7 @@ const tempoObjetivo4 = new Date("2038-12-01T00:00:00")
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function atualizaCronometro() {
-  for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
-  }
-}
 
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date()
@@ -42,14 +35,32 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
+  if(tempoFinal > 0) {
+    
+  }
+
   return (
     dias +
     " dias " +
     horas +
     " horas " +
-    minutos +
+    minutos + 
     " minutos " +
     segundos +
     " segundos"
   );
 }
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro(){
+  
+  atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
